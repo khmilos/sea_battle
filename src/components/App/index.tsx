7d9b2646a1d34ec77d869bdf44929023a82148d2
khmilos <khmil.oleksandr32@gmail.com>
2021-02-05 19:23:51 +0200

    App Grids Styles

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import GameGrid from 'components/GameGrid';
 import ShipPanel from 'components/ShipPanel';
+import styles from './styles.module.css';
 
 function App() {
   return (
-    <div>
-      <GameGrid gridKey='playerGrid' />
-      <ShipPanel />
+    <div className={styles.container}>
+      <main className={styles.gridWrapper}>
+        <section className={styles.grid}>
+          <GameGrid gridKey='playerGrid' />
+        </section>
+        <section className={styles.grid}>
+          <GameGrid gridKey='playerGrid' />
+        </section>
+      </main>
+      <aside className={styles.board}>
+        <ShipPanel />
+      </aside>
     </div>
   );
 }
