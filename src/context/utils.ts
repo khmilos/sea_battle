1ffe67b7ce9b2b02865065fec7c07b53236ffb87
khmilos <khmil.oleksandr32@gmail.com>
2021-02-05 14:22:16 +0200

    Context Utils

---
+++ src/context/utils.ts
@@ -0,0 +1,25 @@
+import { Ship, Cell } from './types';
+
+export function findShip(shipList: Ship[], cell: Cell) {
+  return shipList.findIndex((ship) => {
+    return ship.find((x) => x[0] === cell[0] && x[1] === cell[1]);
+  });
+}
+
+export function findAdjacentShips(shipList: Ship[], cell: Cell) {
+  return [
+    findShip(shipList, [cell[0], cell[1] + 1]),
+    findShip(shipList, [cell[0], cell[1] - 1]),
+    findShip(shipList, [cell[0] + 1, cell[1]]),
+    findShip(shipList, [cell[0] - 1, cell[1]]),
+  ].filter((x) => x !== -1);
+}
+
+export function isDiagonallyAdjacent(shipList: Ship[], cell: Cell) {
+  return [
+    findShip(shipList, [cell[0] - 1, cell[1] - 1]),
+    findShip(shipList, [cell[0] - 1, cell[1] + 1]),
+    findShip(shipList, [cell[0] + 1, cell[1] - 1]),
+    findShip(shipList, [cell[0] + 1, cell[1] + 1]),
+  ].filter((x) => x !== -1).length > 0;
+}
