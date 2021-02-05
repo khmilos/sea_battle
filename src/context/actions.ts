f603701b0aca4cdb05c582a68de488c601b904e6
khmilos <khmil.oleksandr32@gmail.com>
2021-02-05 15:43:58 +0200

    Context Actions for Placement Stage

---
+++ src/context/actions.ts
@@ -0,0 +1,66 @@
+import {
+  Cell,
+  ContextDispatch,
+  ContextState,
+  EXPAND_SHIP,
+  MERGE_SHIPS,
+  NEW_SHIP,
+  REMOVE_SHIP,
+  SHRINK_SHIP
+} from './types';
+import { findAdjacentShips, findShip, isDiagonallyAdjacent } from './utils';
+
+export function handleGridClick(
+  dispatch: ContextDispatch,
+  state: ContextState
+){
+  return (cell: Cell) => {
+    const { shipList } = state.playerGrid
+    const index = findShip(shipList, cell);
+    
+    // Handle remove
+    if (index !== -1) {
+      if (shipList[index].length === 1) {
+        return dispatch({ type: REMOVE_SHIP, payload: { index } });
+      }
+      const cellIndex = shipList[index].findIndex((x) => {
+        return x[0] === cell[0] && x[1] === cell[1];
+      });
+      return dispatch({
+        type: SHRINK_SHIP,
+        payload: { shipIndex: index, cellIndex },
+      });
+    }
+
+    // Handle adding
+    if (isDiagonallyAdjacent(shipList, cell)) {
+      return Error('Diagonally adjacent');
+    }
+    const indices = findAdjacentShips(shipList, cell);
+    switch (indices.length) {
+      case 0: return dispatch({ type: NEW_SHIP, payload: { cell} });
+      case 1: {
+        if (shipList[indices[0]].length >= 4) {
+          return Error('Maximum length of ship');
+        }
+        return dispatch({
+          type: EXPAND_SHIP,
+          payload: { index: indices[0], cell },
+        });
+      }
+      case 2: {
+        const length = shipList[indices[0]].length
+          + shipList[indices[1]].length
+          + 1;
+        if (length > 4) {
+          return Error('Maximum length of merged ship');
+        }
+        return dispatch({
+          type: MERGE_SHIPS,
+          payload: { indices: (indices as [number, number]), cell },
+        });
+      }
+      default: return Error('Error state');
+    }
+  }
+}
