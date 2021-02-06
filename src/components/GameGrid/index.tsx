e358d0697aa0bcad2fcedc01dc0a505830569a53
khmilos <khmil.oleksandr32@gmail.com>
2021-02-06 08:44:01 +0200

    Refactoring: GameGrid - Created gridInit function

--- src/components/GameGrid/index.tsx
+++ src/components/GameGrid/index.tsx
@@ -1,26 +1,16 @@
 import React, { useContext } from 'react';
 
 import context from 'context';
-import { findShip } from 'context/utils';
 import { gridClick } from 'context/actions';
 import { GridKey } from 'context/types';
 
-import { getClass } from './utils';
-import { CellType } from './types';
+import { initGrid, getClass } from './utils';
 import styles from './styles.module.css';
 
 function GameGrid({ gridKey }: { gridKey: GridKey }) {
   const { state, dispatch } = useContext(context);
   const handleClick = gridClick(dispatch, state, gridKey);
-  const grid = Array(10).fill(null).map((_, i) => {
-    return Array(10).fill(null).map((_, j) => {
-      if (gridKey === 'opponentGrid') return CellType.Unknown;
-      if (findShip(state[gridKey].shipList, [i, j]) === -1) {
-        return CellType.Empty;
-      }
-      return CellType.Ship;
-    });
-  });
+  const grid = initGrid(state[gridKey].shipList, gridKey);
   return (
     <table className={styles.table}>
       <tbody>
--- src/components/GameGrid/utils.ts
+++ src/components/GameGrid/utils.ts
@@ -1,5 +1,16 @@
+import { Ship, GridKey } from 'context/types';
+import { findShip } from 'context/utils';
 import { CellType } from './types';
 import styles from './styles.module.css';
+
+export function initGrid(shipList: Ship[], gridKey: GridKey) {
+  return Array(10).fill(null).map((_, i) => {
+    return Array(10).fill(null).map((_, j) => {
+      if (findShip(shipList, [i, j]) !== -1) return CellType.Ship;
+      return gridKey === 'opponentGrid' ? CellType.Unknown : CellType.Empty;
+    });
+  });
+}
 
 export function getClass(cell: CellType) {
   switch (cell) {
